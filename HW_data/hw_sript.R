5cdfdf4831e7185046091f1f4cd62a2e66a78a54
cam333 <cmohaniii@gmail.com>
2016-02-04 16:20:25 -0500

    updates

--- HW_data/hw_sript.R
+++ HW_data/hw_sript.R
@@ -22,7 +22,7 @@ summary(transactions_raw)
 # lets merge some data
 
 all_data <- left_join(product_class_raw,product_raw, by = "product_class_id") %>%
-  left_join(transactions_raw,data1, by = "product_id")%>%
+  left_join(transactions_raw,., by = "product_id")%>%
   mutate(product_department = gsub("'","",product_department),
          product_category = gsub("'","",product_category),
          product_subcategory = gsub("'","",product_subcategory),
@@ -60,23 +60,35 @@ ggplot(category_data, aes(x = reorder(product_category,total_sales), y = total_s
   ggtitle("Sum of Unit Sales by Product Department by product_category")
 
 
-# by store
+# by sub category
 
 
 sub_category_data <- all_data  %>%
   group_by(product_subcategory) %>%
-  summarize(total_sales = sum(as.numeric(unit_sales), na.rm = TRUE), product_category = last(product_category)) %>%
+  summarize(total_sales = sum(as.numeric(unit_sales), na.rm = TRUE), product_department = last(product_department)) %>%
   na.omit() %>%
-  arrange(product_category)
+  arrange(product_department)
 
+all_letters <- paste(paste(letters,collapse="|"),paste(LETTERS,collapse="|"),sep="|")
 
+promotion_data <- data.frame(promotion_raw,stringsAsFactors = FALSE) %>%
+  mutate(media_type = gsub("'","",media_type),
+         promotion_id = gsub("'","",promotion_id),
+         start_date = gsub("'","",start_date),
+         end_date = gsub("'","",end_date),
+         cost = gsub("'","",cost),
+         promotion_name = gsub("'","",promotion_name),
+         promotion_district_id = gsub("'","",promotion_district_id),
+         all_types = ifelse(grepl(all_letters,as.character(cost)) == FALSE, media_type,paste(media_type,cost,sep=",")),
+         new_date_1 = ifelse(lead(all_types) == "",lead(promotion_id),"false"),
+         new_date_2 = ifelse(lead(promotion_district_id) != "" & lead(all_types) == "", lead(promotion_district_id), "false"),
+         real_start_date = ifelse(new_date_1 == "false" & new_date_2 == 'false',start_date,new_date_1),
+         real_end_date =   ifelse(new_date_1 != "false" & new_date_2 != 'false',new_date_2,end_date),
+         real_cost =  ifelse(lead(promotion_id)!= "" & lead(promotion_district_id) != "" & lead(all_types) == "", end_date,
+                      ifelse(lead(promotion_id)!= "" & lead(promotion_district_id) == "", start_date, cost))) %>%
+  filter(real_start_date != "" & real_start_date != "NULL" ) %>%
+  mutate(all_types = gsub("'","",all_types)) %>%
+  select(-new_date_1,-new_date_2,-start_date,-end_date,-cost)
 
-ggplot(sub_category_data , aes(x = product_subcategory, y = total_sales)) +
-  geom_bar(stat = 'identity',aes(fill = product_category)) +
-  theme(axis.text.x=element_text(angle=90,hjust=1,vjust=0.5)) +
-  coord_flip()+
-  xlab("Product Department") +
-  ylab("Unit Sales") +
-  ggtitle("Sum of Unit Sales by Product Department by product_category")
 
 
